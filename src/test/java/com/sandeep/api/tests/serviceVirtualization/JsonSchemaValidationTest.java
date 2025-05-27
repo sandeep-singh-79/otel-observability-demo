0f5d5f09357e44de1523e0d684dabdfe3077eed4
Sandeep Singh <s.sandeep79@ymail.com>
2025-05-27 17:07:37 +0530

    enhance OpenTelemetry instrumentation in test classes for better trace management

--- src/test/java/com/sandeep/api/tests/serviceVirtualization/JsonSchemaValidationTest.java
+++ src/test/java/com/sandeep/api/tests/serviceVirtualization/JsonSchemaValidationTest.java
@@ -1,17 +1,21 @@
 package com.sandeep.api.tests.serviceVirtualization;
 
-import com.github.fge.jsonschema.SchemaVersion;
-import com.github.fge.jsonschema.cfg.ValidationConfiguration;
-import com.github.fge.jsonschema.main.JsonSchemaFactory;
 import com.sandeep.api.tests.BaseAPITest;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.StatusCode;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
@@ -40,35 +44,59 @@ public class JsonSchemaValidationTest extends BaseAPITest {
     }
 
     @Test
-    // this one works. disabled as apiBase object not initialized to the correct endpoint
-    public void validateJsonSchemaForGetUsers() throws FileNotFoundException {
-        JsonSchemaFactory jsonSchemaFactory = JsonSchemaFactory.newBuilder()
-                .setValidationConfiguration(
-                        ValidationConfiguration.newBuilder()
-                                .setDefaultVersion(SchemaVersion.DRAFTV4).freeze())
-                .freeze();
-
+    public void validateJsonSchemaForGetUsers() throws Exception {
+        Span parentSpan = Span.current();
+        Tracer tracer = GlobalOpenTelemetry.getTracer("api-tests");
+        long startTime = System.currentTimeMillis();
         // Arrange
-        wireMockServer.stubFor(get(urlEqualTo(USERS.toString()))
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withHeader("Content-Type", JSON.toString())
-                        .withBody(jsonBody)));
+        Span arrangeSpan = tracer.spanBuilder("request").setParent(Context.current().with(parentSpan)).startSpan();
+        try (Scope arrangeScope = arrangeSpan.makeCurrent()) {
+            wireMockServer.stubFor(get(urlEqualTo(USERS.toString()))
+                    .willReturn(aResponse()
+                            .withStatus(200)
+                            .withHeader("Content-Type", JSON.toString())
+                            .withBody(jsonBody)));
+            arrangeSpan.addEvent("Stub created for GET /users");
+        } finally { arrangeSpan.end(); }
 
-        // Action
-        try {
-            response = apiBase.get_response(GET, USERS).andReturn();
-        } catch (NullPointerException e) {
-            log.error("Unable to initialize Response object as null was returned!");
-        }
+        // Request/Response
+        Response localResponse = null;
+        Span reqSpan = tracer.spanBuilder("request-response").setParent(Context.current().with(parentSpan)).startSpan();
+        try (Scope reqScope = reqSpan.makeCurrent()) {
+            try {
+                localResponse = apiBase.get_response(GET, USERS).andReturn();
+                reqSpan.addEvent("Request sent", Attributes.of(
+                    AttributeKey.stringKey("request.url"), USERS.toString(),
+                    AttributeKey.stringKey("request.method"), "GET"
+                ));
+                localResponse.getHeaders().asList().stream()
+                    .filter(h -> !h.getName().equalsIgnoreCase("Authorization"))
+                    .forEach(h -> reqSpan.setAttribute("response.header." + h.getName(), h.getValue()));
+                reqSpan.setAttribute("response.status", localResponse.getStatusCode());
+                reqSpan.setAttribute("response.body.length", localResponse.getBody().asString().length());
+            } catch (Exception e) {
+                reqSpan.recordException(e);
+                reqSpan.setStatus(StatusCode.ERROR, "Request step failed");
+                throw e;
+            }
+        } finally { reqSpan.end(); }
 
-        // Assert
-        response
-                .then()
-                .statusCode(200)
-                .assertThat()
-                .body(matchesJsonSchema(new FileInputStream("src/test/resources/test_data/jsonSchema/reqresResponseSchema.json"))
-                        .using(jsonSchemaFactory));
-
+        // Assertion
+        Span assertionSpan = tracer.spanBuilder("assertion").setParent(Context.current().with(parentSpan)).startSpan();
+        try (Scope assertionScope = assertionSpan.makeCurrent()) {
+            try {
+                localResponse
+                        .then()
+                        .statusCode(200)
+                        .assertThat()
+                        .body(matchesJsonSchema(new java.io.File("src/test/resources/test_data/jsonSchema/reqresResponseSchema.json")));
+                assertionSpan.addEvent("Asserted status code 200 and schema validation");
+            } catch (AssertionError ae) {
+                assertionSpan.recordException(ae);
+                assertionSpan.setStatus(StatusCode.ERROR, "Assertion failed");
+                throw ae;
+            }
+        } finally { assertionSpan.end(); }
+        parentSpan.setAttribute("test.execution.time.ms", System.currentTimeMillis() - startTime);
     }
 }
